8e47edce024af347e3cf76019e3523859fc27e43
Isaías Chávez Martínez <isaiaschavez.co@outlook.com>
2025-06-26 14:11:03 -0600

    Initial Commit

---
+++ ArKit/ArKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArKitApp.swift
+//  ArKit
+//
+//  Created by Isaías Chávez Martínez on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ArKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
